7d31ff8536a75b9fcbe2731a6291bff148f668b0
rbpolim <rbpolim@gmail.com>
2023-10-09 11:06:27 -0300

    feat: created user controller

---
+++ src/main/java/br/com/rbpolim/todolist/user/UserController.java
@@ -0,0 +1,33 @@
+package br.com.rbpolim.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * Modificadores
+  - public
+  - protected
+  - private
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+  /*
+   * string (Texto)
+   * int (Número inteiro)
+   * double (Número decimal)
+   * boolean (Verdadeiro ou falso)
+   * float (Número decimal)
+   * char (Caractere)
+   * date (Data)
+   * void
+   */
+
+  @PostMapping("/")
+  public void create(@RequestBody UserModel userModel) {
+    System.out.println(userModel.name);
+  }
+
+}
