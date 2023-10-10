18cd04991f08bca07525244ccd2189b062e99969
rbpolim <rbpolim@gmail.com>
2023-10-10 10:48:49 -0300

    feat: validation user already exists

--- src/main/java/br/com/rbpolim/todolist/user/UserController.java
+++ src/main/java/br/com/rbpolim/todolist/user/UserController.java
@@ -1,5 +1,7 @@
 package br.com.rbpolim.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+  @Autowired // Injeta a dependência automaticamente
+  private IUserRepository userRepository;
+
   /*
    * string (Texto)
    * int (Número inteiro)
@@ -26,7 +32,15 @@ public class UserController {
    */
 
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.getUsername());
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    var userExists = this.userRepository.findByUsername(userModel.getUsername());
+
+    if (userExists != null) {
+      return ResponseEntity.badRequest().body("User already exists");
+    }
+
+    var user = this.userRepository.save(userModel);
+
+    return ResponseEntity.ok(user);
   }
 }
