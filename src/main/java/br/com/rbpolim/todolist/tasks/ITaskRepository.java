ecdb61354022764aed6f60309c7fd91af30a473f
rbpolim <rbpolim@gmail.com>
2023-10-12 10:55:01 -0300

    feat: created new task method into ITaskRepository

--- src/main/java/br/com/rbpolim/todolist/tasks/ITaskRepository.java
+++ src/main/java/br/com/rbpolim/todolist/tasks/ITaskRepository.java
@@ -3,6 +3,8 @@ package br.com.rbpolim.todolist.tasks;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+  List<TaskModel> findAllByIdUser(UUID idUser);
 }
