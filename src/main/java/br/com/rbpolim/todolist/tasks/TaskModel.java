6f43a147dadd005705e340c91fe6222e51abe2a9
rbpolim <rbpolim@gmail.com>
2023-10-13 11:53:02 -0300

    feat: created exception method to task title

--- src/main/java/br/com/rbpolim/todolist/tasks/TaskModel.java
+++ src/main/java/br/com/rbpolim/todolist/tasks/TaskModel.java
@@ -30,4 +30,13 @@ public class TaskModel {
 
   @CreationTimestamp
   private LocalDateTime createdAt;
+
+  // Criando um Exception para ser lançada quando title for > 50 caracteres
+  public void setTitle(String title) throws Exception {
+    if (title.length() > 50) {
+      throw new Exception("Title must be less than 50 characters");
+    }
+
+    this.title = title;
+  }
 }
